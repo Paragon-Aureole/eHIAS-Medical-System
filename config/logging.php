abdcbf7c43fbc3c1203c0caee9dacf20708aa2e2
Paragon_143 <tedd14346@gmail.com>
2019-09-27 18:29:36 +1000

    Added Logging

--- config/logging.php
+++ config/logging.php
@@ -36,15 +36,22 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['stack'],
+			//'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
+		
+		'single' => [
+		
+			'driver' => 'errorlog',
+			'level' => 'debug',
+		],
 
-        'single' => [
+        /*'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
-        ],
+        ],*/
 
         'daily' => [
             'driver' => 'daily',
